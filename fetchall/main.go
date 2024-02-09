b93206c9af395119446340ddd434ddd19b570f2c
Daniil <ksusonic@yandex.ru>
2024-02-10 00:00:44 +0300

    fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,41 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+func main() {
+	var wg sync.WaitGroup
+	start := time.Now()
+	var elapsed time.Duration
+
+	for _, url := range os.Args[1:] {
+		wg.Add(1)
+		go func(url string, t time.Time) {
+			defer wg.Done()
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			duration := time.Since(start)
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+			fmt.Printf("%s\t%d\t%s\n", duration, len(body), url)
+			elapsed = duration
+		}(url, start)
+	}
+	wg.Wait()
+	fmt.Println(elapsed, "elapsed")
 }
