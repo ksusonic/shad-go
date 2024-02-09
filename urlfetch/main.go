aa865ce861e5d464e2c8ca32f473134743b3e0a8
Daniil <ksusonic@yandex.ru>
2024-02-09 23:59:48 +0300

    utlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,24 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+	}
 }
