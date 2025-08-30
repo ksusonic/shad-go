239bc58365a1222083bf4bae46275766b0325f6c
Daniil <ksusonic@yandex.ru>
2025-08-30 17:46:58 +0300

    utf-8 tasks

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,24 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	builder := strings.Builder{}
+	builder.Grow(len(input))
+
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		if r == utf8.RuneError && size == 1 {
+			// невалидный байт → заменяем
+			r = '\uFFFD'
+		}
+		builder.WriteRune(r)
+		input = input[:len(input)-size]
+	}
+
+	return builder.String()
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,37 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var res strings.Builder
+	res.Grow(len(input))
+
+	ongoingSpace := false
+
+	for _, char := range input {
+		if ongoingSpace {
+			if unicode.IsSpace(char) {
+				continue
+			}
+
+			res.WriteRune(' ')
+			ongoingSpace = false
+		}
+
+		if unicode.IsSpace(char) {
+			ongoingSpace = true
+		} else {
+			res.WriteRune(char)
+		}
+	}
+
+	if ongoingSpace {
+		res.WriteRune(' ')
+	}
+
+	return res.String()
 }
